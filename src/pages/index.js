f1eeac33d8b17aa9f2cbc796a143d841e5df50d9
mini-mirana <mini_mirana@yahoo.com>
2022-02-24 22:33:41 +0100

    feat: add tailwind configuration

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Portfolio</title>
+        <meta name='description' content='Portfolio' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+    </div>
+  )
+}
