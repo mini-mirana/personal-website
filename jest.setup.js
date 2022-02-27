e57484d96e0d155fb097f27d1b6959a68d0dbce6
ALi <s.arme@ya.ru>
2022-02-27 17:48:14 +0100

    test(unit): Fix bug
    Remove importing expect from '@playwright/test' for unit tests

--- jest.setup.js
+++ jest.setup.js
@@ -2,6 +2,5 @@
 
 import '@testing-library/jest-dom/extend-expect'
 import { toMatchImageSnapshot } from 'jest-image-snapshot'
-import { expect } from '@playwright/test'
 
 expect.extend({ toMatchImageSnapshot })
