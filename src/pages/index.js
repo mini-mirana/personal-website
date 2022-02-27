134d4efea90ea6bcfb0ba42726b34eca979a72e6
mini-mirana <mini_mirana@yahoo.com>
2022-02-27 17:02:20 +0100

    feat(components): :sparkles: Remove unnecessary parts of the code, Add font as an argument for Text component
    Text component now recieves font argument (it's no more hard coded)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import * as THREE from 'three'
-import React, { Suspense, useRef, useState, useCallback } from 'react'
+import React, { Suspense, useRef /* useState, useCallback */ } from 'react'
 import { Canvas, useThree, useFrame } from '@react-three/fiber'
 import { useAspect, Html } from '@react-three/drei'
 import { EffectComposer, Bloom } from '@react-three/postprocessing'
@@ -8,7 +8,6 @@ import { Flex, Box } from '@react-three/flex'
 import { Text } from '../components/Text'
 import { BackGrid } from '../components/BackGrid'
 import { Title } from '../components/Title'
-import { RotatingObj } from '../components/RotatingObj'
 // import { Reflower } from '../components/Reflower'
 
 const state = {
@@ -33,159 +32,57 @@ export function Cube() {
   )
 }
 
-function Page({ onChangePages }) {
+function Page(/* { onChangePages } */) {
   const group = useRef()
   const { size } = useThree()
   const [vpWidth, vpHeight] = useAspect(size.width, size.height)
   const vec = new THREE.Vector3()
   useFrame(() => group.current.position.lerp(vec.set(0, 0, state.top / 100), 0.1))
-  const handleReflow = useCallback(
-    (w, h) => {
-      onChangePages(h / vpHeight)
-      // console.log({ h, vpHeight, pages: h / vpHeight });
-    },
-    [onChangePages, vpHeight]
-  )
+  // const handleReflow = useCallback(
+  //   (w, h) => {
+  //     onChangePages(h / vpHeight)
+  //     // console.log({ h, vpHeight, pages: h / vpHeight });
+  //   },
+  //   [onChangePages, vpHeight]
+  // )
 
   return (
     <group ref={group}>
       <BackGrid />
       <Flex
-        flexDirection='column'
         size={[vpWidth, vpHeight, 0]}
-        onReflow={handleReflow}
+        // onReflow={handleReflow}
         position={[-vpWidth / 2, vpHeight / 2, 0]}>
-        {/* <Reflower /> */}
-
         <Title />
-
-        <group position-z={-0.3}>
-          <Box
-            flexDirection='row'
-            alignItems='center'
-            justifyContent='center'
-            flexWrap='wrap'
-            width='100%'
-            marginTop={0.3}
-            marginBottom={0.1}>
-            <Box centerAnchor>
-              <RotatingObj />
-            </Box>
-            <Box marginLeft={0.3}>
-              <Text fontSize={0.4} maxWidth={1} textAlign='center'>
-                Flexing some Layout
-                <meshStandardMaterial />
-              </Text>
-            </Box>
-          </Box>
-          <Box
-            flexDirection='row'
-            alignItems='center'
-            justifyContent='center'
-            flexWrap='wrap'
-            width='100%'
-            marginTop={0.1}
-            marginBottom={0.5}>
-            <Box marginLeft={0.3}>
-              <Text fontSize={0.4} maxWidth={vpWidth} textAlign='center'>
-                with REACT THREE FLEX
-                <meshStandardMaterial />
-              </Text>
-            </Box>
-          </Box>
-        </group>
-
-        <Box flexDirection='row' alignItems='center' justifyContent='center' flexWrap='wrap' width='100%'>
-          <Box margin={0.05}>
-            <mesh position={[2.5 / 2, -1, 0]}>
-              <planeBufferGeometry args={[2.5, 2]} />
-              <meshStandardMaterial color={['#2d4059', '#ea5455', '#decdc3', '#e5e5e5'][0 % 4]} />
-            </mesh>
-            <Box flexDirection='column' padding={0.1}>
-              <Box marginBottom={0.1} marginLeft={0.05}>
-                <Text fontSize={0.2} letterSpacing={0.1} textAlign='center'>
-                  OUR PRODUCTS
-                  <meshStandardMaterial />
+      </Flex>
+      <Flex
+        size={[vpWidth, vpHeight, 0]}
+        position={[-vpWidth / 2, vpHeight / 2, -8]}
+        alignItems='center'
+        justifyContent='center'>
+        <Box>
+          <Box flexDirection='row' flexWrap='wrap' width={1.8} flexGrow={1}>
+            {[...Array(9).keys()].map((k) => (
+              <Box width={0.5} margin={0.05} key={k}>
+                <mesh position={[0.5 / 2, -0.5 / 2, 0]}>
+                  <planeBufferGeometry args={[0.5, 0.5]} />
+                  <meshStandardMaterial args={[{ transparent: true, opacity: 0.2 }]} />
+                </mesh>
+                <Text
+                  font='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/webfonts/fa-brands-400.woff'
+                  fontSize={0.1}
+                  letterSpacing={0.1}
+                  textAlign='center'
+                  anchorX='center'
+                  position-x={0.5 / 2}
+                  anchorY='middle'
+                  position-y={-0.5 / 2}>
+                  {'\uf3b8'}
+                  <meshStandardMaterial color='#403d39' />
                 </Text>
               </Box>
-              <Box flexDirection='row' flexWrap='wrap' width={2} flexGrow={1}>
-                {[...Array(8).keys()].map((k) => (
-                  <Box margin={0.05} key={k}>
-                    <mesh position={[0.3 / 2, -0.3 / 2, 0]}>
-                      <planeBufferGeometry args={[0.3, 0.3]} />
-                      <meshStandardMaterial />
-                    </mesh>
-                  </Box>
-                ))}
-              </Box>
-            </Box>
+            ))}
           </Box>
-
-          <Box margin={0.05}>
-            <mesh position={[2.5 / 2, -1, 0]}>
-              <planeBufferGeometry args={[2.5, 2]} />
-              <meshStandardMaterial color={['#2d4059', '#ea5455', '#decdc3', '#e5e5e5'][1 % 4]} />
-            </mesh>
-            <Box flexDirection='column' padding={0.1}>
-              <Box marginBottom={0.1} marginLeft={0.05}>
-                <Text fontSize={0.2} letterSpacing={0.1} textAlign='center'>
-                  OUR SERVICES
-                  <meshStandardMaterial />
-                </Text>
-              </Box>
-              <Box flexDirection='row' flexWrap='wrap' width={2} flexGrow={1}>
-                {[...Array(8).keys()].map((k) => (
-                  <Box margin={0.05} key={k}>
-                    <mesh position={[0.3 / 2, -0.3 / 2, 0]}>
-                      <planeBufferGeometry args={[0.3, 0.3]} />
-                      <meshStandardMaterial />
-                    </mesh>
-                  </Box>
-                ))}
-              </Box>
-            </Box>
-          </Box>
-        </Box>
-
-        <group position-z={0.4}>
-          <Box
-            flexDirection='column'
-            alignItems='center'
-            justifyContent='center'
-            width='100%'
-            marginTop={0.8}
-            marginBottom={1}>
-            <Box margin={0.1}>
-              <Text fontSize={0.2} letterSpacing={0.1} maxWidth={vpWidth * 0.8} textAlign='center'>
-                ORDER WITH CONFIDENCE
-                <meshStandardMaterial />
-              </Text>
-            </Box>
-            <Box margin={0.1}>
-              <Text fontSize={0.2} letterSpacing={0.1} maxWidth={vpWidth * 0.8} textAlign='center'>
-                ONE DAY DELIVERY
-                <meshStandardMaterial />
-              </Text>
-            </Box>
-          </Box>
-        </group>
-
-        <Box
-          flexDirection='row'
-          alignItems='center'
-          justifyContent='center'
-          flexWrap='wrap'
-          width='100%'
-          // width="70%"
-        >
-          {[...Array(8 * 4).keys()].map((k, i) => (
-            <Box margin={0.05} key={k}>
-              <mesh position={[0.5, -0.5, 0]}>
-                <planeBufferGeometry args={[1, 1]} />
-                <meshStandardMaterial color={['#2d4059', '#ea5455', '#decdc3', '#e5e5e5'][i % 4]} />
-              </mesh>
-            </Box>
-          ))}
         </Box>
       </Flex>
     </group>
@@ -198,13 +95,14 @@ export default function Home() {
     state.top = e.target.scrollTop
   }
   // useEffect(() => void onScroll({ target: scrollArea.current }), [])
-  const [pages, setPages] = useState(0)
+  // const [pages, setPages] = useState(0)
 
   return (
     <>
       <Head>
         <title>Portfolio</title>
         <meta name='description' content='Portfolio' />
+        <meta httpEquiv='Content-Type' content='text/html; charset=utf-8' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
@@ -225,7 +123,7 @@ export default function Home() {
         />
 
         <Suspense fallback={<Html center>loading..</Html>}>
-          <Page onChangePages={setPages} />
+          <Page /* onChangePages={setPages} */ />
           {/* <Cube /> */}
         </Suspense>
 
@@ -235,7 +133,8 @@ export default function Home() {
       </Canvas>
 
       <div className='absolute top-0 left-0 w-screen h-screen overflow-auto' ref={scrollArea} onScroll={onScroll}>
-        <div style={{ height: `${pages * 100}vh` }} />
+        {/* <div style={{ height: `${pages * 100}vh` }} /> */}
+        <div className='h-[3000px]' />
       </div>
     </>
   )
