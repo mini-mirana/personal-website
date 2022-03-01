b1b35aed3a2e9377c477c6c2c8983c063984b0e6
mini-mirana <mini_mirana@yahoo.com>
2022-03-01 20:54:34 +0100

    feat(Website): :sparkles: Mouse cursor
    A circle follows the mouse cursor

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,64 @@
 import Head from 'next/head'
 import * as THREE from 'three'
-import React, { Suspense, useRef /* , useState, useCallback */ } from 'react'
+import React, { Suspense, useRef, useState /* , useState, useCallback */ } from 'react'
 import { Canvas, useThree, useFrame } from '@react-three/fiber'
-import { useAspect, Html, TrackballControls } from '@react-three/drei'
+import { useAspect, Html, TrackballControls /* useCursor */ } from '@react-three/drei'
 import { EffectComposer, Bloom } from '@react-three/postprocessing'
 import { Flex } from '@react-three/flex'
+import tunnel from 'tunnel-rat'
 import { BackGrid } from '../components/BackGrid'
 import { Title } from '../components/Title'
 import { Grid } from '../components/Grid'
 import { Stack } from '../components/Stack'
 // import { Reflower } from '../components/Reflower'
 
+// Dig a tunnel
+const dom = tunnel()
+
 const state = {
   top: 0
+}
+
+function Cursor({ children, speed = 0.1, eps = 0.001, /* col = new THREE.Color(), */ vec = new THREE.Vector2() }) {
+  const outer = useRef()
+  // const [hovered, hover] = useState()
+  const [pos] = useState(() => new THREE.Vector2())
+  const color = /* hovered ? '#' + col.copy(hovered.material.color).convertLinearToSRGB().getHexString() : */ '#777'
+
+  // useCursor(hovered)
+  useFrame((s) => {
+    vec.set(s.size.width / 2 + (s.mouse.x * s.size.width) / 2, s.size.height / 2 - (s.mouse.y * s.size.height) / 2)
+    if (Math.abs(pos.x - vec.x) > eps || Math.abs(pos.y - vec.y) > eps) {
+      pos.lerp(vec, speed)
+      outer.current.style.transform = `translate3d(${pos.x - 40}px,${pos.y - 40}px,0)`
+    }
+  })
+
+  return (
+    // onPointerOver={(e) => (e.stopPropagation(), hover(e.object))} onPointerOut={(e) => hover(null)}
+    <group>
+      {children}
+      {/* Everything we'll put into the tunnels "In" will be projected
+          outwards to the tunnels "Out". That means we can write dom nodes
+          from within r3f, with full access to canvas state! */}
+      <dom.In>
+        <div ref={outer} className='pointer-events-none absolute left-0 top-0'>
+          <div
+            className='w-[80px] h-[80px] border-2 border-solid border-[orange] rounded-[50%]'
+            style={{
+              transition: `all 350ms cubic-bezier(0, 0.28, 0, 0.77)`,
+              transform: `scale(${/* hovered ? 1 : */ 0.25})`,
+              borderWidth: /* hovered ? '10px' : */ '40px',
+              borderColor: color
+            }}
+          />
+          {/* <div class="cursor-text" style={{ background: color, opacity: hovered ? 1 : 0 }}>
+            {color}
+          </div> */}
+        </div>
+      </dom.In>
+    </group>
+  )
 }
 
 export function Cube() {
@@ -170,10 +216,15 @@ export default function Home() {
           shadow-mapSize-height={1024}
         />
 
-        <Suspense fallback={<Html center>loading..</Html>}>
-          <Page /* onChangePages={setPages} */ />
-          {/* <Cube /> */}
-        </Suspense>
+        <Cursor speed={0.05}>
+          {/* <Sphere color="orange" position={[-1, 0, 0]} scale={0.25} />
+          <Sphere color="aquamarine" position={[0, 0, 0]} scale={0.25} />
+          <Sphere color="tomato" position={[1, 0, 0]} scale={0.25} /> */}
+          <Suspense fallback={<Html center>loading..</Html>}>
+            <Page /* onChangePages={setPages} */ />
+            {/* <Cube /> */}
+          </Suspense>
+        </Cursor>
 
         <EffectComposer>
           <Bloom luminanceThreshold={0.3} luminanceSmoothing={0.9} height={1024} />
@@ -181,6 +232,8 @@ export default function Home() {
 
         <TrackballControls noRotate zoomSpeed={0.2} />
       </Canvas>
+      {/* This is the tunnels "Out", contents will appear here (we're in react-dom, not r3f) */}
+      <dom.Out />
     </>
   )
 }
