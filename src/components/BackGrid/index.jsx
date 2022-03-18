b5ff1507c0d1f915caf65896bf5ee6d62d2c1b71
mini-mirana <mini_mirana@yahoo.com>
2022-03-18 15:19:34 +0100

    feat: :sparkles: Make the bakgrid Cylindrical

--- src/components/BackGrid/index.jsx
+++ src/components/BackGrid/index.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 import { useThree } from '@react-three/fiber'
-import { Plane } from '@react-three/drei'
+// import { Plane } from '@react-three/drei'
 import * as THREE from 'three'
 
 export function BackGrid() {
@@ -10,9 +10,13 @@ export function BackGrid() {
   }, [scene])
 
   return (
-    <Plane position={[0, -1, 152]} rotation={[Math.PI / 2, 0, 0]} args={[80, 80, 128, 128]}>
+    // <Plane position={[0, -1, 152]} rotation={[Math.PI / 2, 0, 0]} args={[80, 80, 128, 128]}>
+    //   <meshStandardMaterial color='#ea5455' wireframe side={THREE.DoubleSide} />
+    // </Plane>
+    <mesh rotation={[Math.PI / 2, 0, 0]} position={[0, -1, 152]}>
+      <cylinderGeometry args={[5, 5, 500, 256, 256, true, 0, 6.283185307179586]} />
       <meshStandardMaterial color='#ea5455' wireframe side={THREE.DoubleSide} />
-    </Plane>
+    </mesh>
   )
 }
 
