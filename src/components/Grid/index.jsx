723756f2f4601950452c7bfc6e9fd9802472d9ba
mini-mirana <mini_mirana@yahoo.com>
2022-03-18 15:32:11 +0100

    fix(components): :bug: Make the Grid component center

--- src/components/Grid/index.jsx
+++ src/components/Grid/index.jsx
@@ -31,7 +31,7 @@ export function Grid({
   return (
     <Flex
       size={[vpWidth, vpHeight, 0]}
-      position={[-vpWidth / 2, vpHeight / 2 + 0.3, 152]}
+      position={[-vpWidth / 2, vpHeight / 2, 152]}
       alignItems='center'
       justifyContent='center'
       name='.Grid'>
