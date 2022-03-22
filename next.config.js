3498097fd30a63689b7f4c98cbf27ef9dcb0f060
ALi <s.arme@ya.ru>
2022-03-22 15:28:36 +0100

    build(components): Use index.js as the offline fallback

--- next.config.js
+++ next.config.js
@@ -2,10 +2,20 @@ const withPWA = require('next-pwa')
 
 /** @type {import('next').NextConfig} */
 module.exports = withPWA({
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en'
+  },
   pwa: {
-    dest: 'public'
+    dest: 'public',
+    fallbacks: {
+      document: '/index'
+    },
+    cacheOnFrontEndNav: true
     // disable: process.env.NODE_ENV === 'development',
   },
   reactStrictMode: true,
-  experimental: {}
+  experimental: {
+    outputStandalone: true
+  }
 })
