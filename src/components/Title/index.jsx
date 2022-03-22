4f29ad34fc8da350ae552bbb89eda6bcdcd9ac13
mini-mirana <mini_mirana@yahoo.com>
2022-03-22 19:21:52 +0100

    fix(components): :bug: Get content as input

--- src/components/Title/index.jsx
+++ src/components/Title/index.jsx
@@ -1,29 +1,19 @@
 import { Flex, Box } from '@react-three/flex'
 import { Text } from '../Text'
 
-export function Title({ ...props }) {
+export function Title({ content = [], ...props }) {
   return (
     <group rotation={[0, 0, Math.PI]}>
       <Flex {...props}>
         <Box flexDirection='column' alignItems='center' justifyContent='center' width='100%' height='100%'>
-          <Box margin={0.05}>
-            <Text fontSize={0.5} letterSpacing={0.1} textAlign='center'>
-              REACT
-              <meshStandardMaterial />
-            </Text>
-          </Box>
-          <Box margin={0.05}>
-            <Text fontSize={0.5} letterSpacing={0.1} textAlign='center'>
-              THREE
-              <meshStandardMaterial />
-            </Text>
-          </Box>
-          <Box margin={0.05}>
-            <Text fontSize={0.5} letterSpacing={0.1} textAlign='center'>
-              FIBER
-              <meshStandardMaterial />
-            </Text>
-          </Box>
+          {content.map((c) => (
+            <Box margin={0.05}>
+              <Text fontSize={0.5} letterSpacing={0.1} textAlign='center'>
+                {c}
+                <meshStandardMaterial />
+              </Text>
+            </Box>
+          ))}
         </Box>
       </Flex>
     </group>
