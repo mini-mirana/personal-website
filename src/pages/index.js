aa92786d46ba37d4888c8c8df86e5e9c415be21d
ALi <s.arme@ya.ru>
2022-04-11 22:47:47 +0200

    fix(website): Capitalize skills

--- src/pages/index.js
+++ src/pages/index.js
@@ -448,7 +448,7 @@ export default function Home() {
           ]}
           sections={[
             { section: 'Intro', objName: '.Title' },
-            { section: 'skills', objName: '.Grid' },
+            { section: 'Skills', objName: '.Grid' },
             { section: 'Experience', objName: '.Stack' },
             { section: 'Papers', objName: '.Paper' }
           ]}
